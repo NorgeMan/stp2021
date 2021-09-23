5f15d45be1d5e299e96f3e468d235bb159c07f18
NorgeMan <ivan.veremeichik@gmail.com>
2021-09-23 17:38:57 +0300

    Rectangle class changes

--- labs/lab01/lab01_oop.py
+++ labs/lab01/lab01_oop.py
@@ -4,22 +4,49 @@ class C:
 
 obj = C()
 
+
 class Rectangle:
-    default_color = "green"
+    def __new__(cls, *args, **kwargs):
+        print("Hello from __new__")
+        return super().__new__(cls)
+
+    #    default_color = "green"
 
     def __init__(self, width, height):
+        print("Hello from __init__")
         self.width = width
         self.height = height
+
+    def get_width(self):
+        return self._width
+
+    def set_width(self, w):
+        self._width = w
+
+    def get_height(self):
+        return self._height
+
+    def set_width(self, h):
+        self._height = h
+
+    def area(self):
+        return self.width * self.height
+
+
+rect = Rectangle(10, 20)
+
 # print(Rectangle.default_color)
-r1 = Rectangle(1, 2)
-r2 = Rectangle(10, 20)
+# r1 = Rectangle(1, 2)
+# r2 = Rectangle(10, 20)
+#
+# r1.default_color = "blue"
+# Rectangle.default_color = "red"
 
-r1.default_color = "blue"
-Rectangle.default_color = "red"
-#print(r1.default_color, r2.default_color)
-#print(Rectangle.default_color)
 
-#add methods
+# print(r1.default_color, r2.default_color)
+# print(Rectangle.default_color)
+
+# add methods
 
 class MyClass:
 
@@ -33,6 +60,7 @@ class MyClass:
 
     def ex_method(self):
         print("method")
+
 
 MyClass.ex_static_method()
 
