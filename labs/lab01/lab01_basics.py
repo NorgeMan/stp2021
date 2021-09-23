b0983aa0da976e0a7b1905421f19d214e077d358
NorgeMan <ivan.veremeichik@gmail.com>
2021-09-23 13:17:26 +0300

    calc v1

--- labs/lab01/lab01_basics.py
+++ labs/lab01/lab01_basics.py
@@ -1,6 +1,18 @@
+from colorama import init
+from colorama import Fore, Back, Style
+
+init()
+print(Fore.BLACK)
+print(Back.GREEN)
+
 what = input("What to do?(+,-): ")
+
+print(Back.CYAN)
+
 a = float(input("Enter the first number: "))
 b = float(input("Enter the second number: "))
+
+print(Back.YELLOW)
 
 if what == "+":
     c = a + b
