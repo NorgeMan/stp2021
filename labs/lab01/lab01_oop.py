7a7d5d1e5449e1033e956e0a572c7b60ea1fecf2
NorgeMan <ivan.veremeichik@gmail.com>
2021-09-23 17:22:58 +0300

    methods added

--- labs/lab01/lab01_oop.py
+++ labs/lab01/lab01_oop.py
@@ -16,5 +16,27 @@ r2 = Rectangle(10, 20)
 
 r1.default_color = "blue"
 Rectangle.default_color = "red"
-print(r1.default_color, r2.default_color)
-print(Rectangle.default_color)
+#print(r1.default_color, r2.default_color)
+#print(Rectangle.default_color)
+
+#add methods
+
+class MyClass:
+
+    @staticmethod
+    def ex_static_method():
+        print("static method")
+
+    @classmethod
+    def ex_class_method(cls):
+        print("class method")
+
+    def ex_method(self):
+        print("method")
+
+MyClass.ex_static_method()
+
+MyClass.ex_class_method()
+
+m = MyClass()
+m.ex_method()
